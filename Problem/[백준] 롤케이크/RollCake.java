33ba631ba3ba58aa088f0caf3ed093711c2a7b91
SoHyeon <69255463+KuSohyeon@users.noreply.github.com>
2020-08-22 23:59:53 +0900

    반복문

--- Problem/[백준] 롤케이크/RollCake.java
+++ Problem/[백준] 롤케이크/RollCake.java
@@ -30,31 +30,30 @@ public class RollCake {
 			P = sc.nextInt();
 			K = sc.nextInt();
 			
-			expected = K-P;
-			if(expectedMax<expected) {
+			expected = K-P+1;//받기로 기대 될 케이크 수 
+			if(expectedMax<expected) {//만약 max값보다 현재 값이더 크면 바꿔주기
 				expectedMax = expected;
-				cakeman = i;
+				cakeman = i;//그리고 케이크 받는 방청객의 인덱스값 넣어주기
 			}
 			for(int j=P;j<=K;j++) {
-				if(rollcake[j]!= 0) {
+				if(rollcake[j]!= 0) {//케이크가 0이 아니면 이미 다른 방청객한테 할당된 경우니까 continue
 					continue;
 				}
 				rollcake[j]=i;
 			}
 		}
 		
-		cnt = new Data[N+1];
-		cnt[0]=new Data();
+		cnt = new Data[N+1]; 
+		cnt[0]=new Data();//1부터 넣어줄꺼니까 얘는 앞으로 빼주기
 		for(int j=1;j<=N;j++) {
-			cnt[j]=new Data();
+			
 			int count=0;
 			for(int i=1;i<L;i++) {
 				if(rollcake[i]==j) {
 					count++;
 				}
 			}
-			cnt[j].index = j;
-			cnt[j].cnt = count;
+			cnt[j]=new Data(j,count);
 		}
 		Arrays.sort(cnt);
 		
