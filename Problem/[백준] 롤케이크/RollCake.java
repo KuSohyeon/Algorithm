dc4e48a4d57afb6f5c2bf8628607869e5f0d25ea
SoHyeon <69255463+KuSohyeon@users.noreply.github.com>
2020-08-23 00:05:20 +0900

    반복문

--- Problem/[백준] 롤케이크/RollCake.java
+++ Problem/[백준] 롤케이크/RollCake.java
@@ -18,54 +18,53 @@ public class RollCake {
 	static Data [] cnt;
 	static int [] rollcake;
 	static int L,N,P,K,expected,expectedMax,cakeman;
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		L = sc.nextInt();//롤케이크 길이
 		rollcake = new int[L+1]; // 1부터 시작
 		N = sc.nextInt(); //사람 수
-		
+
+		cnt = new Data[N+1]; 
+		cnt[0]=new Data();//1부터 넣어줄꺼니까 얘는 앞으로 빼주기
 		for(int i=1;i<=N;i++) {
 			P = sc.nextInt();
 			K = sc.nextInt();
-			
+
 			expected = K-P+1;//받기로 기대 될 케이크 수 
+			
 			if(expectedMax<expected) {//만약 max값보다 현재 값이더 크면 바꿔주기
 				expectedMax = expected;
 				cakeman = i;//그리고 케이크 받는 방청객의 인덱스값 넣어주기
 			}
+			
 			for(int j=P;j<=K;j++) {
 				if(rollcake[j]!= 0) {//케이크가 0이 아니면 이미 다른 방청객한테 할당된 경우니까 continue
 					continue;
 				}
 				rollcake[j]=i;
 			}
-		}
-		
-		cnt = new Data[N+1]; 
-		cnt[0]=new Data();//1부터 넣어줄꺼니까 얘는 앞으로 빼주기
-		for(int j=1;j<=N;j++) {
 			
 			int count=0;
-			for(int i=1;i<L;i++) {
-				if(rollcake[i]==j) {
+			for(int j=1;j<L;j++) {
+				if(rollcake[j]==i) {
 					count++;
 				}
 			}
-			cnt[j]=new Data(j,count);
+			cnt[i]=new Data(i,count);
 		}
 		Arrays.sort(cnt);
-		
+
 		System.out.println(cakeman);
 		System.out.println(cnt[0].index==0?cnt[1].index:cnt[0].index);
 
-		
+
 	}
 	static class Data implements Comparable<Data>{
 		int index;
 		int cnt;
-		
+
 		public Data() {}
 		public Data(int index, int cnt) {
 			super();
