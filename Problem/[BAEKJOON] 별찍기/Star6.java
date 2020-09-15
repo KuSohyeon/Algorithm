c5181c1aff21f8613a1fa05460cb5d70b06321df
kusohyeon <kusohyeon@gmail.com>
2020-09-16 01:29:56 +0900

    for

---
+++ Problem/[BAEKJOON] 별찍기/Star6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Star6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int N = sc.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			for(int z=0;z<i;z++) {
+				sb.append(" ");
+			}
+			for(int j=2*(N-1-i);j>=0;j--) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
