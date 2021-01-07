3018d0a9c98d7867fc1c51d23738caf045425d2a
SoHyeon <69255463+KuSohyeon@users.noreply.github.com>
2021-01-07 22:00:11 +0900

    그리디

--- Problem/[BAEKJOON] 오셀로재배치/RelocationOthello.java
+++ Problem/[BAEKJOON] 오셀로재배치/RelocationOthello.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+public class RelocationOthello {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
